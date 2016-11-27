1ff624bf8f3f5e8c5b550e8d8873e3ef29fe6794
Godfrey Nolan <godfrey@riis.com>
2016-11-27 18:05:58 -0500

    Feature 2 - pass

--- app/src/main/java/com/riis/zodiac/ZodiacDetailActivity.java
+++ app/src/main/java/com/riis/zodiac/ZodiacDetailActivity.java
@@ -14,5 +14,21 @@ public class ZodiacDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zodiac_detail);
+
+        int signNum = (Integer) getIntent().getExtras().get(EXTRA_SIGN);
+        Zodiac zodiac = Zodiac.signs[signNum];
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(zodiac.getName());
+
+        TextView description = (TextView) findViewById(R.id.description);
+        description.setText(zodiac.getDescription());
+
+        TextView symbol = (TextView) findViewById(R.id.symbol);
+        symbol.setText(zodiac.getSymbol());
+
+        TextView month = (TextView) findViewById(R.id.month);
+        month.setText(zodiac.getMonth());
+
     }
 }
